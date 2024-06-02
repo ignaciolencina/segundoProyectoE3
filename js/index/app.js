427af02c7e6be743e1c4cf062bfb07eee32fabfa
Ignacio Lencina <nacholencina60@gmail.com>
2024-06-02 01:08:49 -0300

    Agrego filtrado de busqueda

--- js/index/app.js
+++ js/index/app.js
@@ -1,10 +1,51 @@
 import { obtenerPeliculaDeLS } from "../utils.js";
 import { cargarCard } from "./utils.js";
 
+const $seccionPeliculasBuscadas = document.getElementById(
+  "seccion-peliculas-buscadas"
+);
 const peliculas = obtenerPeliculaDeLS();
 
 // Carga de las peliculas/series en el body
 
 peliculas.forEach((pelicula) => {
+  if (pelicula.publicada === true){
     cargarCard(pelicula);
+  }
+  
+});
+
+// Busqueda
+
+const $formBusqueda = document.getElementById("form-index-peliculas");
+const $inputBusqueda = document.getElementById("input-busqueda-peliculas");
+
+$formBusqueda.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $seccionPeliculasBuscadas.innerHTML = "";
+
+  const busqueda = $inputBusqueda.value.trim();
+
+  if (busqueda !== "") {
+    const peliculaFiltrada = peliculas.filter((pelicula) => {
+      return (
+        pelicula.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
+        pelicula.categoria.toLowerCase().includes(busqueda.toLowerCase())
+      );
+    });
+
+    peliculaFiltrada.forEach((pelicula) => {
+      const $a = document.createElement("a");
+      $a.href = "#";
+      $a.className = "text-light text-decoration-none ms-2 d-block";
+      $a.innerText = `${pelicula.nombre} / ${pelicula.categoria}`;
+      $seccionPeliculasBuscadas.appendChild($a);
+    });
+  } else {
+    const $mensaje = document.createElement("p");
+    $mensaje.className = "text-light ms-2"
+    $mensaje.innerText = "Por favor, ingrese un título o categoría";
+    $seccionPeliculasBuscadas.appendChild($mensaje);
+  }
 });
